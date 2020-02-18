0e7078a528658f2fd1d64f1f0844721c9fc9a4cc
gjy gjy <xusysh@126.com>
2020-02-18 23:58:15 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect:'/login'
+    },
+    {
+      path: '/login',
+      name:'Login',
+      component:Login
+    }
+  ]
+})
