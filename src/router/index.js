7390a1169a4d1a45ecc294dde806e736f0208752
gjy gjy <xusysh@126.com>
2020-02-19 23:50:45 +0800

    upadte

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login/Login'
 import User from '@/components/User/User'
+import DocumentManagement from '@/components/User/DocumentManagement/DocumentManagement'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
@@ -20,7 +22,12 @@ export default new Router({
     {
       path: '/user',
       name:'User',
-      component:User
+      component:User,
+      children:[{
+        path:'document-management',
+        name:'DocumentManagement',
+        component:DocumentManagement
+      }]
     }
   ]
 })
